7bea29d1115e9dc0dd311c3c145d2c7c798cc4aa
eduardoer <edu_6al@hotmail.com>
2023-08-28 21:16:13 -0300

    projeto

---
+++ app/Models/Lancamento.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+use App\Models\{
+     CentroCusto,
+     Tipo,
+     User
+};
+
+class Lancamento extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'lancamentos';
+    protected $primaryKey = 'id_lancamento';
+
+    protected $date = [
+        'create_at',
+        'update_at',
+        'delete_at',
+        'vencimento'
+    ];
+
+    protected $fillable = [
+        'id_user',
+        'id_tipo',
+        'id_centro_custo',
+        'valor',
+        'vencimento',
+        'descricao',
+        'anexo'
+     ];
+
+     protected $cast = [
+        'vencimento' => 'date',
+        'valor' => 'decimal'
+     ];
+
+     /**
+     * ------------------------------------
+     * | Relacionamentos
+     * | https://laravel.com/docs/10.x/eloquent-relationships
+     * ------------------------------------
+     */
+
+     public function tipo()
+     {
+        return $this->belongsTo(
+            Tipo::class,
+            'id_tipo',
+            'id_tipo'
+        );
+     }
+
+     public function centroCusto()
+     {
+       return $this->belongsTo(
+        CentroCusto::class,
+        'id_centro_custo',
+        'id_centro_custo'
+       );
+     }
+
+     public function usuario()
+     {
+        return $this->belongsTo(
+            User::class,
+            'id',
+            'id_user'
+        );
+
+     }
+
+
+     /**
+     * --------------------------
+     * | Mutators
+     * | https://laravel.com/docs/10.x/eloquent-mutators
+     * --------------------------
+     */
+
+     protected function descricao(): Attribute
+     {
+         return Attribute::make(
+             get: fn (string $value) => ucfirst($value),
+             set: fn (string $value) => strtolower(trim($value)),
+         );
+     }
+
+    /**
+     * --------------------------------------
+     * | Outros metodos
+     * |
+     * --------------------------------------
+     */
+
+
+}
